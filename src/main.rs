3550d8bfb1d8c29e3e794032e0ee32fc07d59d69
JerryDing <deanroom@gmail.com>
2024-03-28 22:13:12 +0800

    add serial simulation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
-use std::time::Duration;
-
-use serialport;
+use chrono::{Local, Timelike};
+use std::{
+    sync::mpsc::{self, Receiver},
+    thread,
+    time::{Duration, SystemTime},
+};
 fn main() {
-    read_devices();
-    // let port = serialport::new("/dev/ttys011", 9600)
-    // .timeout(Duration::from_millis(10))
-    // .open().expect("Failed to open port");
-}
+    let (tx, rx): (mpsc::Sender<String>, Receiver<String>) = mpsc::channel();
+    let (tx_up, rx_up): (mpsc::Sender<String>, Receiver<String>) = mpsc::channel();
 
-fn read_devices(){
-    let ports = serialport::available_ports().expect("No ports found!");
-    for p in ports {
-        println!("{}", p.port_name);
-    }
+    let read_thread = thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(1));
+        let command = "FFDD01EE0B0200006400140032050177".to_string();
+        println!("发送数据: {},{}", command, milliseconds());
+        tx.send(command).unwrap();
+        let rx_str = rx_up.recv().unwrap();
+        println!("收到数据: {},{}", rx_str,milliseconds());
+    });
+    run_serial(rx, tx_up);
+    read_thread.join().unwrap();
 }
-
-fn read_data(device: &str){
-    println!("Reading data from device: {}", device);
+fn milliseconds() -> String {
+    let now = Local::now();
+    format!(
+        "{:02}:{:02}:{:02}.{:03}",
+        now.hour(),
+        now.minute(),
+        now.second(),
+        now.nanosecond() / 1_000_000
+    )
 }
-fn set_data(device: &str){
-    println!("Setting data from device: {}", device);
+fn run_serial(rx: Receiver<String>, tx: mpsc::Sender<String>) {
+    let _ = thread::spawn(move || {
+        loop {
+            let received = rx.recv().unwrap();
+            let received_str = received.as_str();
+            match received_str {
+                "FFDD01EE0B0200006400140032050177" => {
+                    tx.send("FF01XXDD005000000000XX".to_string()).unwrap();
+                }
+                // 模拟向串口写入数据
+                _ => println!("未知数据: {}", received_str),
+            }
+        }
+    });
 }
